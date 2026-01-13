89d4b7387e50bab6d7efcd2e28b5d77660ce68ea
nniel-ape <nazandr72@gmail.com>
2026-01-13 15:30:25 +0300

    Initial Commit

---
+++ TunnelMaster/TunnelMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunnelMasterApp.swift
+//  TunnelMaster
+//
+//  Created by Андрей on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TunnelMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
